75dad18129a09196238e396509d41f4beed08c3d
Etamar-Shaviv <54585979+Etamar-Shaviv@users.noreply.github.com>
2019-11-12 01:14:12 +0200

    Update index.js
    :)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function setupWebcam() {
         navigatorAny.msGetUserMedia;
     if (navigator.getUserMedia)
     {
-      navigator.getUserMedia({video: true},      
+      navigator.getUserMedia({video: true,facingMode: 'environment'},
         stream => {
           webcamElement.srcObject = stream;
           webcamElement.addEventListener('loadeddata',  () => resolve(), false);
